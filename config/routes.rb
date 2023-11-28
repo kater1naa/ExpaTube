06acbe01c53901af84d0b162a16810b04f4064b5
Katerina Mastrogianni <katerinamastrog@gmail.com>
2023-11-28 16:15:50 +0100

    save

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
- 
+  root to: "categories#index"
+
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
