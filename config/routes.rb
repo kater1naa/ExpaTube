3e0fff5ee87aac91ebed6b8e27c7c32c95e81c8f
Katerina Mastrogianni <katerinamastrog@gmail.com>
2023-11-28 17:12:27 +0100

    changed the root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "categories#index"
 
+  root to: "pages#home"
+  resources :events, only: [:show, :new, :index, :create]
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
