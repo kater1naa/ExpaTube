693c9c192d88a0a1e2952c613f9d6c2dcf944607
Katerina Mastrogianni <katerinamastrog@gmail.com>
2023-11-29 13:02:32 +0100

    made datetime seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,22 +39,22 @@ Category.create!(name: "Concerts")
 # meetups =
 Category.create!(name: "Other Meetups")
 
- Event.create!(title: "Chill & Connect", user: user1, address: "Rheinlanddamm 200 44139 Dortmund", description: "The perfect setting for those seeking a relaxed and friendly environment to meet new people.", category: casual, limit: 15, starts_at: DateTime.new(2023, 12, 1, 10, 0, 0), ends_at: DateTime.new(2023, 12, 1, 12, 0, 0))
+Event.create!(title: "Chill & Connect", user: user1, address: "Rheinlanddamm 200 44139 Dortmund", description: "The perfect setting for those seeking a relaxed and friendly environment to meet new people.", category: casual, limit: 15, starts_at: DateTime.new(2023, 12, 9, 10, 0, 0), ends_at: DateTime.new(2023, 12, 9, 12, 0, 0))
 
- Event.create!(title: "Casual Conversations", user: user2, address: "Maritim Platz 1 40474 Düsseldorf", description: "The go-to event for those who appreciate the beauty of unhurried, spontaneous dialogue in a laid-back setting.", category: casual, limit: 13)
+Event.create!(title: "Casual Conversations", user: user2, address: "Maritim Platz 1 40474 Düsseldorf", description: "The go-to event for those who appreciate the beauty of unhurried, spontaneous dialogue in a laid-back setting.", category: casual, limit: 13, starts_at: DateTime.new(2023, 12, 13, 18, 0, 0), ends_at: DateTime.new(2023, 12, 13, 20, 0, 0))
 
- Event.create!(title: "Friendly Faces", user: user3, address: "Landfermannstraße 6 47051 Duisburg", description: "Is your invitation to an event where warm smiles and welcoming personalities set the stage for meaningful connections.", category: casual, limit: 20)
+Event.create!(title: "Friendly Faces", user: user3, address: "Landfermannstraße 6 47051 Duisburg", description: "Is your invitation to an event where warm smiles and welcoming personalities set the stage for meaningful connections.", category: casual, limit: 20, starts_at: DateTime.new(2023, 12, 1, 17, 0, 0), ends_at: DateTime.new(2023, 12, 1, 20, 0, 0))
 
- Event.create!(title: "Kickback Conversations", user: user4, address: "Carlswerkstraße 11c 51063 Köln", description: "Invites you to unwind and engage in laid-back discussions in a relaxed and comfortable atmosphere.", category: casual, limit: 12)
+Event.create!(title: "Kickback Conversations", user: user4, address: "Carlswerkstraße 11c 51063 Köln", description: "Invites you to unwind and engage in laid-back discussions in a relaxed and comfortable atmosphere.", category: casual, limit: 12, starts_at: DateTime.new(2023, 12, 1, 10, 0, 0), ends_at: DateTime.new(2023, 12, 1, 12, 0, 0))
 
- Event.create!(title: "Neighborly Natter", user: user5, address: "Martinstraße 39 50667 Köln", description: "We celebrate the beauty of local connections.", category: casual, limit: 50)
+Event.create!(title: "Neighborly Natter", user: user5, address: "Martinstraße 39 50667 Köln", description: "We celebrate the beauty of local connections.", category: casual, limit: 50, starts_at: DateTime.new(2023, 12, 29, 20, 0, 0), ends_at: DateTime.new(2023, 12, 29, 23, 0, 0))
 
- Event.create!(title: "Fitness Fusion", user: user6, address: "Further Straße 27 41462 Neuss", description: "Dynamic and innovative event that brings together the best elements of various fitness disciplines, creating a unique and energizing experience.", category: sports, limit: 16)
+Event.create!(title: "Fitness Fusion", user: user6, address: "Further Straße 27 41462 Neuss", description: "Dynamic and innovative event that brings together the best elements of various fitness disciplines, creating a unique and energizing experience.", category: sports, limit: 16, starts_at: DateTime.new(2024, 1, 9, 10, 0, 0), ends_at: DateTime.new(2024, 1, 9, 12, 0, 0))
 
- Event.create!(title: "Basketball Game", user: user7, address: "Ludolf-Camphausen-Straße 22, 50672 Köln", description: "With a limit of 10 people for 2 teams, drinks and food included.", category: sports, limit: 10)
+Event.create!(title: "Basketball Game", user: user7, address: "Ludolf-Camphausen-Straße 22, 50672 Köln", description: "With a limit of 10 people for 2 teams, drinks and food included.", category: sports, limit: 10, starts_at: DateTime.new(2023, 12, 16, 11, 0, 0), ends_at: DateTime.new(2023, 12, 16, 13, 0, 0))
 
- Event.create!(title: "Swimming Lessons", user: user8, address: "Gürzenichstraße 6-16, 50667 Köln", description: "I invite you to spend a day with the best swimming lessons given by a professional with 10 years of experience.", category: sports, limit: 5)
+Event.create!(title: "Swimming Lessons", user: user8, address: "Gürzenichstraße 6-16, 50667 Köln", description: "I invite you to spend a day with the best swimming lessons given by a professional with 10 years of experience.", category: sports, limit: 5, starts_at: DateTime.new(2024, 1, 14, 16, 0, 0), ends_at: DateTime.new(2024, 1, 14, 17, 0, 0))
 
- Event.create!(title: "Yoga Sessions", user: user9, address: "Am Rinkenpfuhl 27-29 50676 Köln", description: "Immerse yourself in an oasis of calmness, where each breath becomes a mindful movement and every stretch contributes to the harmonious balance of mind, body, and soul.", category: sports, limit: 8)
+Event.create!(title: "Yoga Sessions", user: user9, address: "Am Rinkenpfuhl 27-29 50676 Köln", description: "Immerse yourself in an oasis of calmness, where each breath becomes a mindful movement and every stretch contributes to the harmonious balance of mind, body, and soul.", category: sports, limit: 8, starts_at: DateTime.new(2023, 12, 11, 15, 0, 0), ends_at: DateTime.new(2023, 12, 11, 16, 30, 0))
 
- Event.create!(title: "Football Game", user: user10, address: "Subbelrather Str. 17, 50823 Köln", description: "Two teams limit to spend the day, with good atmosphere and energy. only soccer lovers.", category: sports, limit: 22)
+Event.create!(title: "Football Game", user: user10, address: "Subbelrather Str. 17, 50823 Köln", description: "Two teams limit to spend the day, with good atmosphere and energy. only soccer lovers.", category: sports, limit: 22, starts_at: DateTime.new(2023, 12, 17, 10, 0, 0), ends_at: DateTime.new(2023, 12, 17, 12, 0, 0))
