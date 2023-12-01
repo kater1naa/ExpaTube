c75e70d5a5cb51a9976cd4cfaae12a42c0bcf8ec
Katerina Mastrogianni <katerinamastrog@gmail.com>
2023-12-01 13:06:11 +0100

    made dashboard

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,6 +2,7 @@ class AttendancesController < ApplicationController
   before_action :authenticate_user! # Add this line if you want to ensure that only authenticated users can access these
   def index
     @attendances = current_user.attendances.includes(:event)
+    @events_as_host = current_user.events
   end
 
   def create
